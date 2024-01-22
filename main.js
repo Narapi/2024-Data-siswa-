396195eae3e4e8dd9704b373cc25b529afb50c4d
mrappi752@gmail.com <mrappi752@gmail.com>
2024-01-22 11:14:40 +0700

    menambah konfigurasi firebase

---
+++ main.js
@@ -0,0 +1,25 @@
+import { initializeApp } from " https://www.gstatic.com/firebasejs/10.7.2/firebase-app.js ";
+
+import { 
+  getFirestore,
+  collection,
+  addDoc,
+  getDocs,
+  deletDoc,
+  doc,
+  query,
+  orderBy
+  } from " https://www.gstatic.com/firebasejs/10.7.2/ firebase-firebsetore.js ";
+
+
+const firebaseConfig = {
+  apiKey: "AIzaSyAQhFoRf_4ecIp-b7pclOk29ncMXpjzELE",
+  authDomain: "insan-cemerlang-58879.firebaseapp.com",
+  projectId: "insan-cemerlang-58879",
+  storageBucket: "insan-cemerlang-58879.appspot.com",
+  messagingSenderId: "704155633968",
+  appId: "1:704155633968:web:42d7eeff6bb87ae7af60f5",
+  
+};
+
+const app = initializeApp(firebaseConfig);
